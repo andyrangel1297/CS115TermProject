2f4518aa1a7d28c78d7336f0167b7d12ab24cf77
Andy Rangel <77663935+andyrangel1297@users.noreply.github.com>
2023-04-05 15:28:51 -0700

    draft of some header files for sorting algorithms

---
+++ bubble.h
@@ -0,0 +1,21 @@
+// bubble sort header file
+#include <iostream>
+
+class bubbleSort {
+public:
+    // constructor
+    bubbleSort();
+
+    // destructor
+    ~bubbleSort();
+
+    // sort function
+    void sort(int arr[], int n);
+    // swap function
+    void swap(int *xp, int *yp);
+
+    // print function
+    void printArray(int arr[], int size);
+
+};
+
---
+++ experiment.cpp
@@ -0,0 +1,14 @@
+// file that runs algorithms from header files
+
+#include <iostream>
+
+// include header files for all the sorts/algorithms
+#include <bubble.h>
+
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!" << endl;
+}
