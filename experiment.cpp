f7dec879459a93ed797e883789f3f81454c5a992
Andy Rangel <andyrangel1297@gmail.com>
2023-04-28 12:42:24 -0700

    Deleted all headers & added selection on main

--- experiment.cpp
+++ experiment.cpp
@@ -1,26 +1,79 @@
-// file that runs algorithms from header files
-
 #include <iostream>
+
 using namespace std;
 
-// include header files for all the sorts/algorithms
-#include "insertion.h"
-#include "selection.h"
-#include "bubble.h"
-#include "merge.h"
-#include "quicksort.h"
-#include "heapsort.h"
-#include "countingsort.h"
-#include "radixsort.h"
+
+// function prototype for sorting algorithms
+void selectionSort(int arr[], int size);
+
+// function for returning original unsorted array using reference
+void createOriginalArray(int arr[], int size);   
+
+
 
 // main function
 int main() {
 
     // create an array with 10 elements
     int arr[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int size = 10;
 
-    // get size of array
-    int size = sizeof(arr)/sizeof(arr[0]);
+    // print out unsorted array
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl << endl;
 
-    // call insertion sort function with array and size and array passed by reference
-    insertionSort(arr, size);
+    // SELECTION SORT FUNCTION #1 ---------------------------------------------
+    selectionSort(arr, size);
+
+    // print out sorted array to check selection sort
+    cout << "Array after selection sort: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl << endl;
+
+    // call function that creates original unsorted array
+    createOriginalArray(arr, size);
+
+    // print original array
+    cout << "\nOriginal array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+// function for returning original unsorted array
+void createOriginalArray(int arr[], int size) {
+    int originalArr[10] = {0};
+
+    for (int i = 0; i < size; i++) {
+        originalArr[i] = arr[i];
+    }
+}
+
+// SELECTION SORT ALGORITHM #1 -------------------------------------------------
+void selectionSort(int arr[], int size) {
+    int i, j, indexSmallest, temp = 0;
+
+    for (i = 0; i < size - 1; i++) {
+        indexSmallest = i;
+
+        for (j = i + 1; j < size; j++) {
+
+            if (arr[j] < arr[indexSmallest]) {
+                indexSmallest = j;
+            }
+        }
+
+        // swap function
+        temp = arr[i];
+        arr[i] = arr[indexSmallest];
+        arr[indexSmallest] = temp;
+    }
+}
+
--- insertion.h
+++
@@ -1,6 +0,0 @@
-// insertion sort header file
-
-// declaration of insertion sort function with array passed by reference
-void insertionSort(int (&arr)[10], int size);
-
-
